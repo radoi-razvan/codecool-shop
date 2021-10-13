43a490a9a553a2571322bbc5ec9147c5d79eb6fe
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-10-14 00:12:14 +0300

    implemented shopping cart front end

--- src/Codecool.CodecoolShop/wwwroot/js/site.js
+++ src/Codecool.CodecoolShop/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const init = () => {
+    const toggleCartBtn = document.getElementById("cartBtn");
+    toggleCartBtn.addEventListener("mouseover", (event) => {
+        const cartContainer = document.getElementById("container-cart");
+        cartContainer.style.display = "flex";
+        event.preventDefault();
+    });
+
+    const cartContent = document.getElementById("container-cart").childNodes[1];
+    cartContent.addEventListener("mouseleave", (event) => {
+        const cartContainer = document.getElementById("container-cart");
+        cartContainer.style.display = "none";
+    });
+}
+
+init();
