953c2b5480705f35b321f9091fd824f1f1a08f56
AlexM <51971295+AlexMoraru97@users.noreply.github.com>
2021-10-14 15:20:38 +0300

    Create CartService.cs

---
+++ src/Codecool.CodecoolShop/Services/CartService.cs
@@ -0,0 +1,36 @@
+﻿using Codecool.CodecoolShop.Daos;
+using Codecool.CodecoolShop.Daos.Implementations;
+using Codecool.CodecoolShop.Models;
+using System.Collections.Generic;
+
+namespace Codecool.CodecoolShop.Services
+{
+    public class CartService
+    {
+        private readonly CartDaoMemory cartDao;
+        private readonly IProductCategoryDao productCategoryDao;
+        private readonly ISupplierDao supplierDao;
+
+        public CartService(CartDaoMemory cartDao, IProductCategoryDao productCategoryDao, ISupplierDao supplierDao)
+        {
+            this.cartDao = cartDao;
+            this.productCategoryDao = productCategoryDao;
+            this.supplierDao = supplierDao;
+        }
+
+        public Cart GetCart()
+        {
+            return cartDao.cart;
+        }
+
+        public IEnumerable<ProductCategory> GetAllCategories()
+        {
+            return productCategoryDao.GetAll();
+        }
+
+        public IEnumerable<Supplier> GetAllSuppliers()
+        {
+            return supplierDao.GetAll();
+        }
+    }
+}
